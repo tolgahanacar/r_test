830250f7e74d5877c2c3cde927931506880f679b
tolgahanacar <47753083+tolgahanacar@users.noreply.github.com>
2021-08-02 12:48:04 +0300

    toBeDisabled()
    button, input, link -> test toBeDisabled();

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-        <h1>React_Test</h1>
+        {/* <h1>React_Test</h1>
         <button>Test Button</button>
         <h3 className="rtest">this color is blue!</h3>
-        <p>Coding by Tolgahan</p>
+        <p>Coding by Tolgahan</p> */}
+        <button data-testid="r_button" type="submit" disabled>r_sub</button>
+        <fieldset disabled><input type="text" data-testid="r_input" /></fieldset>
+        <a data-testid="r_link" href="https://tolgahanacar.net"  disabled>r_link</a>
+
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { getByTestId, render, screen } from "@testing-library/react";
 import App from "./App";
 
 beforeAll(() => {
@@ -10,33 +10,45 @@ beforeEach(() => {
   console.log("Before Each");
 });
 
-afterAll(()=>{
+afterAll(() => {
   console.log("After All");
 });
 
-afterEach(()=>{
+afterEach(() => {
   console.log("After Each");
   //cleanup(); -- old
 });
 
-/* access dom */
-test("Header render correctly", () => {
-  const headerElement = screen.getByText(/React_Test/i);
-  expect(headerElement).toBeInTheDocument(); //The title fits the document correctly.
-  expect(headerElement).toHaveTextContent("React_Test");
-});
-
-test("Button Element render correctly", () => {
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toHaveTextContent("Test Button");
-  //expect(buttonElement).toBeDisabled(); //Test Fail, Button is disable or enable?
-  expect(buttonElement).toBeEnabled(); //Test Success, Button is disable or enable?
-});
-
-test("Blue Element render correctly", () => {
-  const blueElement = screen.getByText(/this color is blue/i);
-  expect(blueElement).toBeInTheDocument();
-  expect(blueElement).toHaveTextContent("this color is blue");
-  expect(blueElement).toHaveClass("rtest");
+test("toBeDisabled", () => {
+  const rbutton = screen.getByTestId("r_button"); //get id
+  const rinput  = screen.getByTestId("r_input");
+  const rlink   = screen.getByTestId("r_link");
+  // or 
+  // const rlink   = screen.getByText("r_link");
+  expect(rbutton).toBeDisabled();
+  expect(rinput).toBeDisabled();
+  expect(rlink).not.toBeDisabled();
   screen.debug();
 });
+
+/* access dom */
+// test("Header render correctly", () => {
+//   const headerElement = screen.getByText(/React_Test/i);
+//   expect(headerElement).toBeInTheDocument(); //The title fits the document correctly.
+//   expect(headerElement).toHaveTextContent("React_Test");
+// });
+
+// test("Button Element render correctly", () => {
+//   const buttonElement = screen.getByRole("button");
+//   expect(buttonElement).toHaveTextContent("Test Button");
+//   //expect(buttonElement).toBeDisabled(); //Test Fail, Button is disable or enable?
+//   expect(buttonElement).toBeEnabled(); //Test Success, Button is disable or enable?
+// });
+
+// test("Blue Element render correctly", () => {
+//   const blueElement = screen.getByText(/this color is blue/i);
+//   expect(blueElement).toBeInTheDocument();
+//   expect(blueElement).toHaveTextContent("this color is blue");
+//   expect(blueElement).toHaveClass("rtest");
+//   screen.debug();
+// });
