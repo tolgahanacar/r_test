9712f5b74953150f45aafd66ae896e47d6b3c67c
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2024-10-07 12:45:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+
 const App = () => {
   return (
     <div className="App">
-        {/* <h1>React_Test</h1>
-        <button>Test Button</button>
-        <h3 className="rtest">this color is blue!</h3>
-        <p>Coding by Tolgahan</p> */}
         <button data-testid="r_button" type="submit" disabled>r_sub</button>
-        <fieldset disabled><input type="text" data-testid="r_input" /></fieldset>
-        <a data-testid="r_link" href="https://tolgahanacar.net"  disabled>r_link</a>
+        
+        <fieldset disabled>
+          <input type="text" data-testid="r_input" />
+        </fieldset>
 
+        {/* Link elementine disabled eklenemez, onun yerine tıklama olayını engelle */}
+        <a data-testid="r_link" href="https://tolgahanacar.net" onClick={(e) => e.preventDefault()}>
+          r_link
+        </a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
