ef79f7eab898ed68f8174fb5de7130d2a66bb6a4
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2024-12-02 15:16:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
+// src/App.js
 import React from 'react';
 import './App.css';
 
 const App = () => {
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+    console.log("Link tıklama devre dışı bırakıldı.");
+  };
+
   return (
     <div className="App">
-        <button data-testid="r_button" type="submit" disabled>r_sub</button>
-        
-        <fieldset disabled>
-          <input type="text" data-testid="r_input" />
-        </fieldset>
+      {/* Buton */}
+      <button data-testid="r_button" type="submit" disabled>
+        r_sub
+      </button>
 
-        {/* Link elementine disabled eklenemez, onun yerine tıklama olayını engelle */}
-        <a data-testid="r_link" href="https://tolgahanacar.net" onClick={(e) => e.preventDefault()}>
-          r_link
-        </a>
+      {/* Input alanı disabled */}
+      <fieldset disabled>
+        <input type="text" data-testid="r_input" placeholder="Giriş devre dışı" />
+      </fieldset>
+
+      {/* Link için tıklamayı engelle */}
+      <a
+        data-testid="r_link"
+        href="https://tolgahanacar.net"
+        onClick={handleLinkClick}
+        style={{ color: 'gray', pointerEvents: 'none', cursor: 'default' }}
+      >
+        r_link
+      </a>
     </div>
   );
-}
+};
 
 export default App;
